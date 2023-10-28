319ecdd6c4b4196d2a262a471c3a035e416eee61
Govind Sen <govindsen0915@gmail.com>
2023-10-28 14:48:31 +0530

    refactor: code refactor

--- DemoProject/MoviesModel.swift
+++ DemoProject/MoviesModel.swift
@@ -7,37 +7,6 @@
 
 import Foundation
 
-//struct MoviesModel: Decodable {
-//    let title: String?
-//    let year: String?
-//    let imdbID: String?
-//    let type: String?
-//    let poster: String?
-//
-//    enum CodingKeys: String, CodingKey {
-//        case title = "Title"
-//        case year = "Year"
-//        case poster = "Poster"
-//    }
-//}
-
-
-//struct MoviesModel: Decodable {
-//    let title: String?
-//    let year: String?
-//    let imdbID: String?
-//    let type: String?
-//    let poster: String?
-//
-//    enum CodingKeys: String, CodingKey {
-//        case title = "Title"
-//        case year = "Year"
-//        case imdbID = "imdbID"
-//        case type = "Type"
-//        case poster = "Poster"
-//    }
-//}
-
 struct MoviesModel: Decodable {
     let Search: [Movie]
     let totalResults: String
@@ -46,11 +15,11 @@ struct MoviesModel: Decodable {
 }
 
 struct Movie: Decodable {
-    let title: String
-    let year: String
-    let imdbID: String
-    let type: String
-    let poster: String
+    let title: String?
+    let year: String?
+    let imdbID: String?
+    let type: String?
+    let poster: String?
 }
 
 enum CodingKeys: String, CodingKey {
